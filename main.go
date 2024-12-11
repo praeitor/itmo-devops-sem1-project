2dc49800195a13a22030d5d82bf8bb556f4c86b8
Bulgakov Denis <bulgakovdenis@KapMac770-M.local>
2024-12-11 15:11:28 +0400

    database initialize

--- main.go
+++ main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbUser     = "validator"
+	dbPassword = "val1dat0r"
+	dbName     = "project-sem-1"
+	dbHost     = "localhost"
+	dbPort     = 5432
+)
+
+var db *sql.DB
+
+// Инициализация базы данных
+func initDB() {
+	var err error
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable",
+		dbUser, dbPassword, dbName, dbHost, dbPort)
+	db, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS prices (
+		id SERIAL PRIMARY KEY,
+		product_id INT,
+		created_at DATE,
+		product_name TEXT,
+		category TEXT,
+		price FLOAT
+	)`)
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
+
+	fmt.Println("Database initialized successfully")
+}
