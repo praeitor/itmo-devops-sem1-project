e6b465211b1a70618baab5e53f6f4fc1ccec230f
Bulgakov Denis <bulgakovdenis@KapMac770-M.local>
2024-12-11 15:39:10 +0400

    add logig to the run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+ssh user@<server_ip> << 'EOF'
+sudo apt update
+sudo apt install -y golang git postgresql
+git clone <repo_url>
+cd <repo_folder>
+go build -o app main.go
+nohup ./app &
+EOF
