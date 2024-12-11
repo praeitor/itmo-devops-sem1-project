1c4a458cb2f1c0151e05b4d0fc72b7c754f60e43
Bulgakov Denis <bulgakovdenis@KapMac770-M.local>
2024-12-11 15:38:33 +0400

    add logit to prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+sudo apt update
+sudo apt install -y postgresql postgresql-contrib
+sudo -u postgres psql -c "CREATE USER validator WITH PASSWORD 'val1dat0r';"
+sudo -u postgres psql -c "CREATE DATABASE project-sem-1;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE project-sem-1 TO validator;"
+sudo systemctl restart postgresql
