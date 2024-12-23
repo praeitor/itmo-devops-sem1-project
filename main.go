05018a242549ee7015b1bff2b2dad2945ec7987f
Denis Bulgakov <praeitor@gmail.com>
2024-12-24 00:22:33 +0400

    init and db connect

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func initDB() {
+	var err error
+	connStr := "user=validator password=val1dat0r dbname=project-sem-1 sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Database connected successfully")
+}
+
+func main() {
+	initDB()
+}
