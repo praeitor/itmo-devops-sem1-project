227929aefb3fc4f237fcde96a1827154ac5d311a
Denis Bulgakov <praeitor@gmail.com>
2024-12-24 00:46:40 +0400

    add prepare and run scripts

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+sudo apt update
+sudo apt install -y postgresql golang
+sudo systemctl start postgresql
+sudo -u postgres psql -c "CREATE USER validator WITH PASSWORD 'val1dat0r';"
+sudo -u postgres psql -c "CREATE DATABASE project-sem-1;"
+sudo -u postgres psql -d project-sem-1 -c "GRANT ALL PRIVILEGES ON DATABASE project-sem-1 TO validator;"
+sudo -u postgres psql -d project-sem-1 -c "CREATE TABLE prices (id SERIAL PRIMARY KEY, name TEXT, category TEXT, price NUMERIC(10,2), create_date DATE);"
